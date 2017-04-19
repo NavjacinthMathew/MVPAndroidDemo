0debd0f4326d37a359905805a9aa75e4f0c65857
Navjacinth Mathew <jacinth9@gmail.com>
2017-04-19 19:30:45 +0530

    -Added MVP structure -Moved files into appropriate packages

--- app/src/main/java/com/navjacinthmathew/mvpdemo/view/activity/MainActivity.java
+++ app/src/main/java/com/navjacinthmathew/mvpdemo/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.navjacinthmathew.mvpdemo;
+package com.navjacinthmathew.mvpdemo.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.navjacinthmathew.mvpdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
