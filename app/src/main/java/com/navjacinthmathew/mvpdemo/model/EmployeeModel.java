36c57f555add6837c8db7b0fc86c579238670da7
Navjacinth Mathew <jacinth9@gmail.com>
2017-04-21 10:50:34 +0530

    -Added employee model class

--- app/src/main/java/com/navjacinthmathew/mvpdemo/model/EmployeeModel.java
+++ app/src/main/java/com/navjacinthmathew/mvpdemo/model/EmployeeModel.java
@@ -5,4 +5,37 @@ package com.navjacinthmathew.mvpdemo.model;
  */
 
 public class EmployeeModel {
+    private int id;
+    private String name;
+    private String emailId;
+
+    public EmployeeModel(int id, String name, String emailId) {
+        this.id = id;
+        this.name = name;
+        this.emailId = emailId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 }
