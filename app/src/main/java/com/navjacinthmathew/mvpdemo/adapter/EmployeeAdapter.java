273e7d26210be000aeda52a7361c01ceae355233
Navjacinth Mathew <jacinth9@gmail.com>
2017-04-24 20:18:40 +0530

    -Added adapter for RecyclerView

--- app/src/main/java/com/navjacinthmathew/mvpdemo/adapter/EmployeeAdapter.java
+++ app/src/main/java/com/navjacinthmathew/mvpdemo/adapter/EmployeeAdapter.java
@@ -1,8 +1,56 @@
 package com.navjacinthmathew.mvpdemo.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.navjacinthmathew.mvpdemo.R;
+import com.navjacinthmathew.mvpdemo.model.EmployeeModel;
+
+import java.util.ArrayList;
+
 /**
  * Created by Navjacinth Mathew on 4/19/2017.
  */
 
-public class EmployeeAdapter {
+public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeAdapter.EmployeeViewHolder> {
+
+    private ArrayList<EmployeeModel> dataList;
+
+    public EmployeeAdapter(ArrayList<EmployeeModel> dataList) {
+        this.dataList = dataList;
+    }
+
+    @Override
+    public EmployeeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_employee, parent, false);
+        return new EmployeeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(EmployeeViewHolder holder, int position) {
+        holder.txtEmpName.setText(dataList.get(position).getId());
+        holder.txtEmpEmail.setText(dataList.get(position).getName());
+        holder.txtEmpPhone.setText(dataList.get(position).getEmailId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    class EmployeeViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtEmpName, txtEmpEmail, txtEmpPhone;
+
+        EmployeeViewHolder(View itemView) {
+            super(itemView);
+            txtEmpName = (TextView) itemView.findViewById(R.id.txt_emp_id);
+            txtEmpEmail = (TextView) itemView.findViewById(R.id.txt_emp_name);
+            txtEmpPhone = (TextView) itemView.findViewById(R.id.txt_name_email);
+        }
+    }
 }
