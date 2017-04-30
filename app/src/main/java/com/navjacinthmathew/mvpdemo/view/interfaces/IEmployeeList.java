daeda9bbc3b79c295f34a96c94a3ed79b5941eba
Navjacinth Mathew <jacinth9@gmail.com>
2017-04-30 16:49:21 +0530

    -Added new method

--- app/src/main/java/com/navjacinthmathew/mvpdemo/view/interfaces/IEmployeeList.java
+++ app/src/main/java/com/navjacinthmathew/mvpdemo/view/interfaces/IEmployeeList.java
@@ -1,10 +1,14 @@
 package com.navjacinthmathew.mvpdemo.view.interfaces;
 
+import com.navjacinthmathew.mvpdemo.adapter.EmployeeAdapter;
+
 /**
  * Created by Navjacinth Mathew on 4/19/2017.
  */
 
 public interface IEmployeeList {
+
+    void setAdapter(EmployeeAdapter adapter);
 
     void showEmployeeList();
 
